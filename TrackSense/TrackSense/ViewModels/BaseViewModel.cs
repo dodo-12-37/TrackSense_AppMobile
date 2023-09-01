b5c1dcc811245ba62d5bee6e12aafb7e5529bfbe
Loïc Delas <2291685@csfoy.ca>
2023-09-01 11:15:33 -0400

    Classe de base viewModel

---
+++ TrackSense/TrackSense/ViewModels/BaseViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace TrackSense.ViewModels;
+
+public partial class BaseViewModel : ObservableObject
+{
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+    bool isBusy;
+
+    [ObservableProperty]
+    string title;
+
+    public bool IsNotBusy => !IsBusy;
+
+    public BaseViewModel()
+    {
+        ;
+    }
+}
