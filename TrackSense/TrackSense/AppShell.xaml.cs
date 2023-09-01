c67d5904db9b01f5fbcd06d33e01565c42b842aa
Loïc Delas <2291685@csfoy.ca>
2023-09-01 10:44:42 -0400

    Creation solution

---
+++ TrackSense/TrackSense/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TrackSense
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TrackSense/TrackSense/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TrackSense
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
