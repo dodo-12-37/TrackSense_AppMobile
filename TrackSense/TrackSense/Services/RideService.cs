21b971126d0e113f1269c2a264f82347e3272346
Loïc Delas <2291685@csfoy.ca>
2023-09-06 10:32:24 -0400

    Confirmation réception

--- TrackSense/TrackSense/Services/RideService.cs
+++ TrackSense/TrackSense/Services/RideService.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Plugin.BLE.Abstractions.Contracts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,11 +20,18 @@ namespace TrackSense.Services
             _bluetoothService = bluetoothService;
 
             BluetoothObserver bluetoothObserver = new BluetoothObserver(this._bluetoothService,
-                (value) =>
+                async (value) =>
                 {
                     if (value.Type == BluetoothEventType.SENDING_RIDE_DATA)
                     {
                         this.ReceiveRideData(value.rideData);
+                        //IDevice connectedDevice = this._bluetoothService.GetConnectedDevice();
+                        //Guid completedRideServiceUID = new Guid("62ffab64-3646-4fb9-88d8-541deb961192");
+                        //IService completedRideService = await connectedDevice.GetServiceAsync(completedRideServiceUID);
+                        //Guid characteristicIsReceivedUID = new Guid("08382f5e-4b60-11ee-be56-0242ac120002");
+                        //ICharacteristic characteristicIsReceived = await completedRideService.GetCharacteristicAsync(characteristicIsReceivedUID);
+                        //byte[] dataFalse = { 0x00 };
+                        //await characteristicIsReceived.WriteAsync(dataFalse);
                     }
                 });
         }
@@ -32,6 +40,7 @@ namespace TrackSense.Services
         {
             //Enregistrer le rideData dans RideData
             Console.WriteLine("Receiving ride data...");
+
             Console.WriteLine(rideData);
             //Si connexion à internet, envoyer vers API
         }
