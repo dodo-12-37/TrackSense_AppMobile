ca5caa520c435eac0cf5f6e98fbc062d140fc908
binh nguyen <tienbinhnguyen84@gmail.com>
2023-09-20 19:50:32 -0400

    ajouter les class de models

---
+++ TrackSense/TrackSense/Models/CompletedRideModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackSense.Models
+{
+    internal class CompletedRideModel
+    {
+        [DefaultValue("admin")]
+        public string UserLogin { get; set; } = null!;
+
+        [DefaultValue("b0f07b65-3055-4f99-bc09-91829ca16fdb")]
+        public string CompletedRideId { get; set; } = null!;
+        public PlannedRideModel PlannedRideModel { get; set; } = null;
+        public List<CompletedRidePointModel> CompletedRidePointsModel { get; set; } = new List<CompletedRidePointModel>();
+
+        public CompletedRideStatisticsModel Statistics { get; set; } = null;
+
+        public CompletedRideModel()
+        {
+            ;
+        }
+    }
+}
---
+++ TrackSense/TrackSense/Models/CompletedRidePointModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackSense.Models
+{
+    internal class CompletedRidePointModel
+    {
+        [DefaultValue("b0f07b65-3055-4f99-bc09-91829ca16fdb")]
+        public string CompletedRideId { get; set; } = null!;
+        public LocationModel Location { get; set; } = new LocationModel();
+        public int? RideStep { get; set; }
+        public double? Temperature { get; set; }
+        public DateTime DateTime { get; set; } = DateTime.UtcNow;
+
+        public CompletedRidePointModel()
+        {
+            ;
+        }
+
+    }
+}
