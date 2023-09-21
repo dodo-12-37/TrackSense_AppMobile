aae22ecb8549433682dad729d382279f8e19742f
binh nguyen <tienbinhnguyen84@gmail.com>
2023-09-20 20:07:36 -0400

    ajouter property

--- TrackSense/TrackSense/Entities/CompletedRide.cs
+++ TrackSense/TrackSense/Entities/CompletedRide.cs
@@ -1,16 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrackSense.Models;
 
 namespace TrackSense.Entities
 {
     public class CompletedRide
     {
+        //[DefaultValue("b0f07b65-3055-4f99-bc09-91829ca16fdb")]
+        public string UserLogin { get; set; } = null!;
         public Guid CompletedRideId { get; set; }
         public Guid PlannedRideId { get; set; }
         public List<CompletedRidePoint> CompletedRidePoints { get; set; }
+
+        public List<CompletedRidePoint> CompletedRidePoints { get; set; } = new List<CompletedRidePointModel>();
         public CompletedRideStatistics Statistics { get; set; }
+
+        public CompletedRideStatistics Statistics { get; set; } = null;
     }
 }
