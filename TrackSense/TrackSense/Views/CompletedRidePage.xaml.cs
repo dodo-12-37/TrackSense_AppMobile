4a7d7c4599e28d3c12f9d31fb3809ec1d21108d7
Loïc Delas <2291685@csfoy.ca>
2023-09-23 17:08:22 -0400

    Affichage carte et trajet

--- TrackSense/TrackSense/Views/CompletedRidePage.xaml.cs
+++ TrackSense/TrackSense/Views/CompletedRidePage.xaml.cs
@@ -1,4 +1,13 @@
+using Mapsui.Layers;
+using Mapsui.Nts;
+using Mapsui.Nts.Extensions;
+using Mapsui.Projections;
+using Mapsui.Styles;
+using Mapsui.UI.Maui;
+using NetTopologySuite.Geometries;
+using TrackSense.Models;
 using TrackSense.ViewModels;
+using Color = Mapsui.Styles.Color;
 
 namespace TrackSense.Views;
 
@@ -8,5 +17,56 @@ public partial class CompletedRidePage : ContentPage
 	{
 		InitializeComponent();
 		BindingContext = viewModel;
-	}
+    }
+
+	public void DisplayMap(List<CompletedRidePoint> points)
+	{
+        MapControl mapControl = new Mapsui.UI.Maui.MapControl();
+        mapControl.Map?.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
+
+        ILayer lineStringLayer = CreateLineStringLayer(points, CreateLineStringStyle());
+        mapControl.Map.Layers.Add(lineStringLayer);
+
+        double boxSize = lineStringLayer.Extent!.Width > lineStringLayer.Extent.Height ? lineStringLayer.Extent.Width : lineStringLayer.Extent.Height;
+        double resolution = boxSize / 256;
+        mapControl.Map.Home = n => n.CenterOnAndZoomTo(lineStringLayer.Extent!.Centroid, resolution);
+
+        mapControl.Map.Navigator.RotationLock = true;
+        mapContainer.Children.Add(mapControl);
+    }
+
+    private ILayer CreateLineStringLayer(List<CompletedRidePoint> points, IStyle? style = null)
+    {
+        LineString lineString = new LineString(points.Select(p => SphericalMercator.FromLonLat(p.Location.Longitude, p.Location.Latitude).ToCoordinate()).ToArray());
+
+        return new MemoryLayer
+        {
+            Features = new[] { new GeometryFeature { Geometry = lineString } },
+            Name = "LineStringLayer",
+            Style = style
+
+        };
+    }
+
+    private IStyle CreateLineStringStyle()
+    {
+        return new VectorStyle
+        {
+            Fill = null,
+            Outline = null,
+#pragma warning disable CS8670 // Object or collection initializer implicitly dereferences possibly null member.
+            Line = { Color = Color.FromString("Red"), Width = 4 }
+        };
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        if (BindingContext is CompletedRideViewModel viewModel)
+        {
+            CompletedRide rideToDisplay = viewModel.CompletedRide;
+            DisplayMap(rideToDisplay.CompletedRidePoints);
+        }
+    }
 }
